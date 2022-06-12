c7240d69570befc385897b8ed99ae8ffaed7f200
saurabh_bakolia <skbakolia2002@gmail.com>
2022-06-12 16:48:28 +0530

    capitalize funcationality added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import './App.css';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert'
 import React, {useState} from 'react';
 
 function App() {
 
   const [mode, setMode]  = useState('light'); // set mode using this state
+  const [alert, setAlert] = useState(null); // state to show alert 
+
+  // function to show Alert
+  const showAlert = (type,message)=>{
+      setAlert({ 
+          type:type,
+          msg: message
+      })
+  }
 
   // function to change the mode
   const toggleMode = () =>{
     if(mode==='light') {
       setMode('dark');
      document.body.style.backgroundColor = "#334257";
+     showAlert("success", "Dark mode enabled successfully!");
     }else{
       setMode('light');
       document.body.style.backgroundColor = "#EEEEEE";
+      showAlert("success", "Light mode enabled successfully!");
     }
   }
 
   return (
     <>
     <Navbar title = "texUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container my-3">
-      <TextForm heading = "Enter text to analyze.." mode={mode}/>
+      <TextForm heading = "Enter text to analyze.." mode={mode} showAlert={showAlert}/>
     </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,30 +1,33 @@
   import React, {useState}  from 'react'
 
   export default function TextForm(props) {
+    // state of text
+    const [text, setText] = useState("Enter text here....")
 
     // function to handle the textarea text change
     const handleOnChange = (event)=>{
-      // console.log("OnChange triggered");
       setText(event.target.value)
     };
 
     // function to handle the click event 
     const handleUpClick = () =>{
-      // console.log("Uppercase was clicked");
+      console.log("Uppercase was clicked");
       let newText = text.toUpperCase();
+      props.showAlert("success", "Text has been changed to uppercase!");
       setText(newText);
     }
 
     // function to handle the click event 
     const handleLoClick = () =>{
       let newText = text.toLowerCase();
+      props.showAlert("success", "Text has been changed to lowercase!");
       setText(newText);
     }
 
     // function to handle the click event 
     const handleClearClick = () =>{
-      // console.log("Uppercase was clicked");
       let newText = "";
+      props.showAlert("warning", "Text has been cleared!");
       setText(newText);
     }
 
@@ -33,20 +36,34 @@
         let text = document.getElementById("text_Area");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.showAlert("info", "Text has been copied to the clipboard!");
       }
 
-      // function to handle the Copy event 
+      // function to handle the extra space event 
       const handleExtraSpace = () =>{
         console.log("triggered extra space");
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("success", "Extra spaces has been removed!");
+      }
+
+      // function to convert text into capitalize
+      const handleCapitalClick = () =>{
+        let capitalize ="";
+          text.split(" ").forEach(function(word, index, array) {
+            array[index] = (array[index].toLowerCase()).charAt(0).toUpperCase() + (array[index].toLowerCase().slice(1));
+            console.log(array[index]);
+            console.log(array.toString());
+            capitalize = array.toString().replace(/,,/g, ", ");
+            // console.log(capitalize);
+          });
+          setText(capitalize);
       }
       
 
       
 
-    // state of text
-      const [text, setText] = useState("Enter text here....")
+ 
 
     // word count functionality
     let count = 0;
@@ -55,8 +72,6 @@
         count++;
       }
     })
-
-    console.log(count);
   
   
     return (
@@ -64,10 +79,10 @@
         <>
         <div className="mb-3" style={{color:props.mode==='light' ? '#495464':'rgb(212 212 216)'}}>
             <h2 className="mb-2">{props.heading}</h2>
-            <textarea className="form-control" onChange={handleOnChange} style={{backgroundColor:props.mode === 'light'?'#EEEEEE':'#334257',color:props.mode==='light' ? '#495464':'rgb(212 212 216)'}} id="text_Area" rows="6" placeholder={text}></textarea>
+            <textarea className="form-control" onChange={handleOnChange} style={{backgroundColor:props.mode === 'light'?'#EEEEEE':'#334257',color:props.mode==='light' ? '#495464':'rgb(212 212 216)'}} id="text_Area" rows="6" value={text} placeholder={text}></textarea>
             <button className="btn btn-secondary my-2 mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
             <button className="btn btn-secondary my-2 mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
-            <button className="btn btn-secondary my-2 mx-1" onClick={handleLoClick}>Convert to CapitalizeCase</button>
+            <button className="btn btn-secondary my-2 mx-1" onClick={handleCapitalClick}>Convert to CapitalizeCase</button>
             <button className="btn btn-secondary my-2 mx-1" onClick={handleExtraSpace}>Remove Extra Space</button>
             <button className="btn btn-secondary my-2 mx-1" onClick={handleClearClick}>Clear Text</button>
             <button className="btn btn-secondary my-2 mx-1" onClick={handleCopy}>Copy Text</button>
